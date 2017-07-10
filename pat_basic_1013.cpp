f21d4f9ac69f8767c91fc043b95fedb816e07559
hongge831 <541324951@qq.com>
2017-07-10 19:47:35 +0800

    add pat_basic_1013

---
+++ pat_basic_1013.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+#define M 110001
+int Array[M];
+int Prime[10100];
+int main(){
+	int m, n;
+	cin >> m >> n;
+	/*素数打表开始*/
+	Array[0] = Array[1] = 1;//假设数组中0代表是素数，1代表不是素数，数组下标代表素数值
+	for (int i = 2; i*i < M; i++)
+	{
+		if (Array[i] == 1){
+			continue;
+		}
+		for (int j = i*i; j < M; j = j + i)
+		{
+			Array[j] = 1;
+		}
+	}
+	int k = 1;
+	for (int i = 2; i < M; i++)
+	{
+		if (Array[i] == 0){
+			Prime[k++] = i;
+		}
+	}
+	/*素数打表结束*/
+	for (int i = m, k = 1; i <= n; i++, k++)
+	{
+		if (k % 10 == 0){
+			cout << Prime[i]<< endl;
+		}
+		else if (i == n){
+			cout << Prime[i];
+		}
+		else{
+			cout << Prime[i] << " ";
+		}
+	}
+	return 0;
+}
