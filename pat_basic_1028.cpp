5f49817730fab132910abcdfe870b823d49f45a3
hongge831 <541324951@qq.com>
2017-07-26 13:14:18 +0800

    modify 1028

--- pat_basic_1028.cpp
+++ pat_basic_1028.cpp
@@ -46,3 +46,58 @@ int main()
     system("pause");
     return 0; 
 }
+/*上面的是网络上的答案，没有用到结构体，时间稍微好一点，下面是我自己的答案，时间长一点*/
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+struct info{
+	string name;
+	string birthday;
+} infoTemp;
+bool compare(string a, string b){
+	int i = 0;
+	for (i = 0; i<10; i++)
+	{
+		if (i == 4 || i == 7)
+			continue;
+		if (a[i] - b[i]>0)
+			return false;
+		if (a[i] - b[i]<0)
+			return true;
+	}
+	return 0;
+
+}
+int main(){
+	info infoArr, max, min;
+	max.name = "hello";
+	min.name = "fine";
+	min.birthday = "2014/09/06";
+	max.birthday = "1814/09/06";
+	int n, count;
+	count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> infoArr.name >> infoArr.birthday;
+		if (compare("1814/09/05", infoArr.birthday) && compare(infoArr.birthday, "2014/09/07")){
+			count++;
+			if (compare(infoArr.birthday, min.birthday)){
+				min = infoArr;
+			}if (compare(max.birthday, infoArr.birthday)){
+				max = infoArr;
+			}
+		}
+	}
+	if (count >= 1)
+		cout << count << ' ' << min.name << ' ' << max.name;
+	else 
+		cout << '0';
+
+
+	return 0;
+
+
+}
